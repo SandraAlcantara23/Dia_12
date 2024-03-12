6a26ec37bcfc6b707090edea917c4595975aedd8
SandraAlcantara23 <160050434+SandraAlcantara23@users.noreply.github.com>
2024-03-12 17:44:05 -0600

    Add files via upload

---
+++ saltos.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    printf ("Este es un texto \n");
+    printf ("que fue impreso desde c");
+    printf ("\n ");
+    system ("Pause");
+    return 0;
+}
